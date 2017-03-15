0c43386497aaa90bf1da86e68d0e1b3cf36e8f2f
周云方 <yunfangjsj@163.com>
2017-03-15 14:51:23 +0800

    safddfsdf

---
+++ app/src/main/java/app/myapplication/ImageActivity.java
@@ -0,0 +1,16 @@
+package app.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by zhouyunfang on 17/3/15.
+ */
+public class ImageActivity  extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
